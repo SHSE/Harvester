705c41ab68a6a9ec3ab54c27dd5db92be3c8cc69
Sergey Shumov <shumov.ss@gmail.com>
2012-12-14 09:43:52 +0400

    added udp message listener and extended log4j message parser

---
+++ src/Core/Messaging/Parsers/Log4JParserForRemoteMessage.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Harvester.Core.Processes;
+
+namespace Harvester.Core.Messaging.Parsers
+{
+    internal class Log4JParserForRemoteMessage : IParseMessages
+    {
+        private readonly XmlNamespaceManager xmlNamespaceManager;
+        private readonly XmlParserContext xmlParserContext;
+
+        public Log4JParserForRemoteMessage(
+            IRetrieveProcesses processRetriever,
+            IHaveExtendedProperties extendedProperties)
+        {
+            Verify.NotNull(processRetriever, "processRetriever");
+            Verify.NotNull(extendedProperties, "extendedProperties");
+
+            this.xmlNamespaceManager = new XmlNamespaceManager(new NameTable());
+            this.xmlNamespaceManager.AddNamespace("log4j", "http://logging.apache.org/log4j/");
+            this.xmlParserContext = new XmlParserContext(null, this.xmlNamespaceManager, null, XmlSpace.None);
+        }
+
+        public SystemEvent Parse(IMessage message)
+        {
+            Verify.NotNull(message, "message");
+
+            var document = new XmlDocument();
+
+            using (var reader = new XmlTextReader(
+                message.Message ?? string.Empty, XmlNodeType.Element, this.xmlParserContext))
+                document.Load(reader);
+
+            var process = this.GetProcess(document);
+
+            return new SystemEvent {
+                Level = this.GetLevel(document),
+                ProcessName = process.Name,
+                ProcessId = process.Id,
+                Timestamp = message.Timestamp,
+                Thread = this.GetThread(document),
+                Username = this.GetUsername(document),
+                Source = this.GetSource(document),
+                Message = this.GetMessage(document).Trim() + this.GetException(document),
+                RawMessage = new Lazy<string>(() => FormatRawMessage(document)),
+            };
+        }
+
+        private string GetException(XmlNode document)
+        {
+            var exception = this.QuerySingleValue(document, "./log4j:event/log4j:properties/log4j:data[@name='exception']/@value");
+
+            if (!string.IsNullOrEmpty(exception))
+                return Environment.NewLine + " -> " + exception;
+
+            return string.Empty;
+        }
+
+        public Boolean CanParseMessage(String message)
+        {
+            return message != null &&
+                   message.StartsWith("<log4j:event ") &&
+                   message.EndsWith("</log4j:event>") &&
+                   message.Contains("<log4j:data name=\"log4jmachinename\" ");
+        }
+
+        private SystemEventLevel GetLevel(XmlNode document)
+        {
+            var level = this.QuerySingleValue(document, "./log4j:event/@level");
+            switch (level.ToLowerInvariant())
+            {
+                case "fatal":
+                    return SystemEventLevel.Fatal;
+                case "error":
+                    return SystemEventLevel.Error;
+                case "warn":
+                    return SystemEventLevel.Warning;
+                case "info":
+                    return SystemEventLevel.Information;
+                case "debug":
+                    return SystemEventLevel.Debug;
+                default:
+                    return SystemEventLevel.Trace;
+            }
+        }
+
+        private String GetThread(XmlNode document)
+        {
+            return this.QuerySingleValue(document, "./log4j:event/@thread");
+        }
+
+        private String GetUsername(XmlNode document)
+        {
+            return this.QuerySingleValue(document, "./log4j:event/log4j:properties/log4j:data[@name='username']/@value");
+        }
+
+        private String GetMessage(XmlNode document)
+        {
+            return this.QuerySingleValue(document, "./log4j:event/log4j:message/text()");
+        }
+
+        private string GetSource(XmlNode document)
+        {
+            return
+                this.GetMachineName(document) +
+                '\\' +
+                this.QuerySingleValue(document, "./log4j:event/@logger");
+        }
+
+        private string GetMachineName(XmlNode document)
+        {
+            return this.QuerySingleValue(
+                document, "./log4j:event/log4j:properties/log4j:data[@name='log4jmachinename']/@value");
+        }
+
+        private RemoteProcess GetProcess(XmlNode document)
+        {
+            var appInfo = this.QuerySingleValue(
+                document, "./log4j:event/log4j:properties/log4j:data[@name='log4japp']/@value");
+
+            if (string.IsNullOrEmpty(appInfo))
+                return new RemoteProcess();
+
+            appInfo = appInfo.TrimEnd(')');
+
+            var i = appInfo.LastIndexOf('(');
+
+            if (i == -1)
+                return new RemoteProcess();
+
+            return new RemoteProcess {
+                Id = int.Parse(appInfo.Substring(i + 1)),
+                Name = appInfo.Substring(0, i)
+            };
+        }
+
+        private String QuerySingleValue(XmlNode node, String xPath)
+        {
+            var result = node.SelectSingleNode(xPath, this.xmlNamespaceManager);
+
+            return result == null ? String.Empty : result.Value ?? String.Empty;
+        }
+
+        private static String FormatRawMessage(XmlNode document)
+        {
+            var sb = new StringBuilder();
+
+            using (var stringWriter = new StringWriter(sb))
+            using (var xmlTextWriter = new XmlTextWriter(stringWriter))
+            {
+                xmlTextWriter.Formatting = Formatting.Indented;
+
+                document.WriteTo(xmlTextWriter);
+            }
+
+            return sb.ToString();
+        }
+
+        private class RemoteProcess : IProcess
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool HasExited { get; set; }
+            public DateTime? ExitTime { get; set; }
+        }
+    }
+}
---
+++ src/Core/Messaging/Sources/Network/UdpMessageListener.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Harvester.Core.Messaging.Sources.Network
+{
+    public class UdpMessageListener : MessageListener
+    {
+        private readonly UdpServerMessageBuffer messageBuffer;
+
+        public UdpMessageListener(IProcessMessages messageProcessor, IConfigureListeners configuration)
+            : this(
+                GetSource(configuration),
+                messageProcessor,
+                new UdpServerMessageBuffer(GetSource(configuration), GetEndPoint(configuration)))
+        {
+        }
+
+        private UdpMessageListener(
+            string source,
+            IProcessMessages messageProcessor,
+            UdpServerMessageBuffer messageBuffer)
+            : base(source, messageProcessor, new UdpMessageReader(messageBuffer))
+        {
+            this.messageBuffer = messageBuffer;
+        }
+
+        private static IPEndPoint GetEndPoint(IConfigureListeners configuration)
+        {
+            var uri = new Uri(configuration.Binding.Replace("+", "any"));
+
+            if (uri.Scheme != "udp")
+                throw new NotSupportedException("Not suppoerted protocol: " + uri.Scheme);
+
+            var ipAddress = uri.Host == "any" ? IPAddress.Any : IPAddress.Parse(uri.Host);
+
+            return new IPEndPoint(ipAddress, uri.Port);
+        }
+
+        private static string GetSource(IConfigureListeners configuration)
+        {
+            Verify.NotNull(configuration, "configuration");
+
+            return configuration.Binding;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                this.messageBuffer.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private sealed class Log4JMessage : IMessage
+        {
+            public Log4JMessage(string source, byte[] data)
+            {
+                this.Timestamp = DateTime.Now;
+                this.Message = Encoding.UTF8.GetString(data);
+                this.Source = source;
+            }
+
+            public DateTime Timestamp { get; private set; }
+            public int ProcessId { get { return 0; } }
+            public string Message { get; private set; }
+            public string Source { get; private set; }
+        }
+
+        private class UdpMessageReader : IReadMessages
+        {
+            private readonly MessageBuffer messageBuffer;
+
+            public UdpMessageReader(MessageBuffer messageBuffer)
+            {
+                this.messageBuffer = messageBuffer;
+            }
+
+            public IMessage Current { get; private set; }
+
+            public bool ReadNext()
+            {
+                try
+                {
+                    var data = this.messageBuffer.Read();
+
+                    if (data == null)
+                    {
+                        this.Current = null;
+                        return false;
+                    }
+
+                    this.Current = new Log4JMessage(this.messageBuffer.Name, data);
+
+                    return true;
+                } catch (Exception exception)
+                {
+                    if (exception is DecoderFallbackException ||
+                        exception is InvalidDataException ||
+                        exception is OverflowException)
+                        return false;
+
+                    throw;
+                }
+            }
+        }
+
+        private class UdpServerMessageBuffer : MessageBuffer
+        {
+            private readonly UdpClient udpClient;
+            private bool closing;
+
+            public UdpServerMessageBuffer(string name, IPEndPoint endPoint) : base(name)
+            {
+                this.udpClient = new UdpClient(endPoint);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    this.closing = true;
+                    this.udpClient.Close();
+                }
+            }
+
+            protected override byte[] ReadMessage()
+            {
+                var endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                while (!this.closing)
+                {
+                    this.udpClient.Client.ReceiveTimeout = 1000;
+                    try
+                    {
+                        return this.udpClient.Receive(ref endPoint);
+                    } catch (SocketException exception)
+                    {
+                        if (exception.SocketErrorCode != SocketError.TimedOut &&
+                            exception.SocketErrorCode != SocketError.Interrupted)
+                            throw;
+                    }
+                }
+
+                return null;
+            }
+
+            protected override void WriteMessage(byte[] message)
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
